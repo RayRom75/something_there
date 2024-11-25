34dbd8192af86a2d59a0406b8d29bfa80615ee15
RayRom <rayrom@inbox.ru>
2024-11-25 10:53:26 +0300

    Модуль 3 урок 1

--- python/module_3/module_3_1.py
+++ python/module_3/module_3_1.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+    return calls
+
+def string_info(stroka):
+    count_calls()
+
+    return (len(stroka), stroka.upper(), stroka.lower())
+
+def is_contains(stroka, stroks_list):
+    count_calls()
+    stroka = stroka.lower()
+
+    for i in stroks_list:
+        if (stroka == i.lower()):
+            return True
+
+    return False
+
+
+calls = 0
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
